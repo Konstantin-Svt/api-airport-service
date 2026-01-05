6a2a95db03916fbe42b166ff8a92f901501d8ac4
Konstantin S. <kostaho16@gmail.com>
2026-01-05 22:07:34 +0200

    Add urls to whole project

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -27,19 +27,23 @@ from drf_spectacular.views import (
 )
 
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/airport/", include("airport.urls", namespace="airport")),
-    path("api/user/", include("user.urls", namespace="user")),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "api/schema/swagger/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
-    path(
-        "api/schema/redoc/",
-        SpectacularRedocView.as_view(url_name="schema"),
-        name="redoc",
-    ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + debug_toolbar_urls()
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("api/airport/", include("airport.urls", namespace="airport")),
+        path("api/user/", include("user.urls", namespace="user")),
+        path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+        path(
+            "api/schema/swagger/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+        path(
+            "api/schema/redoc/",
+            SpectacularRedocView.as_view(url_name="schema"),
+            name="redoc",
+        ),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + debug_toolbar_urls()
+)
--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,25 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from user.views import UserCreateView, UserManageView
 
 
 app_name = "user"
 
 urlpatterns = [
+    path("register/", UserCreateView.as_view(), name="register"),
+    path("me/", UserManageView.as_view(), name="me"),
+    path(
+        "token/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+    path(
+        "token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
 ]
